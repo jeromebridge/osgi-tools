151288895fefb89b329c25f5fdd0ea2324557825
jeromebridge <jeromebridge@pennassurancesoftware.com>
2014-11-13 22:44:33 -0500

    Working on maven integration tool.

---
+++ osgi.maven.integration/src/main/java/tools/osgi/maven/integration/internal/Activator.java
@@ -0,0 +1,23 @@
+package tools.osgi.maven.integration.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+   @Override
+   public void start( BundleContext context ) throws Exception {
+      try {
+         System.out.println( "DEPLOYED" );
+      }
+      catch( Exception exception ) {
+         throw new RuntimeException( String.format( "Error starting bundle: %s, Error: %s", context.getBundle().getSymbolicName(), exception.getMessage() ), exception );
+      }
+   }
+
+   @Override
+   public void stop( BundleContext context ) throws Exception {
+
+   }
+
+}
---
+++ osgi.maven.integration/src/test/java/tools/osgi/maven/integration/test/TestLoadEclipseLocationFile.java
@@ -0,0 +1,20 @@
+package tools.osgi.maven.integration.test;
+
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+
+import org.junit.Test;
+
+public class TestLoadEclipseLocationFile {
+
+   // NOT WORKING
+   @Test
+   public void testLoadFile() throws Exception {
+      // Fixture
+      final InputStream is = getClass().getResourceAsStream( "/eclipse-location-file" );
+
+      // Call
+      final ObjectInputStream ois = new ObjectInputStream( is );
+      ois.readObject();
+   }
+}
